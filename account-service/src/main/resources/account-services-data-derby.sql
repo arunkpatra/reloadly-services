f2316cda1879a805adff98df0766f0b378220623
Arun Patra <arunkpatra@gmail.com>
2021-04-24 14:15:55 +0530

    Polish

--- account-service/src/main/resources/account-services-data-derby.sql
+++ account-service/src/main/resources/account-services-data-derby.sql
@@ -1,12 +1,3 @@
--- INSERT INTO account_balance_table(account_balance_id, account_balance)
--- VALUES ('d1fe6f0d-420e-4161-a134-9c2342e36c88', 200.5);
---
--- INSERT INTO account_table(uid, account_id, account_balance_id, name, email, phone_number, currency_cd, active)
--- VALUES ('c1fe6f0d-420e-4161-a134-9c2342e36c95', 'f1fe6f0d-420e-4161-a134-9c2342e36c99', 'd1fe6f0d-420e-4161-a134-9c2342e36c88', 'John Doe', 'email@email.com', '+19999999999', 'USD', true);
---
-
-
-
 INSERT INTO account_balance_table(account_balance_id, account_balance)
 VALUES ('d1fe6f0d-420e-4161-a134-9c2342e36c88', 200.5);
 
