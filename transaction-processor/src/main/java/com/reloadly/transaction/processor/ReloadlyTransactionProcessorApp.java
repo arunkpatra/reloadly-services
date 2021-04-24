ca7ed03d91bf7fde1302fe2c6e16d89fd8bfaada
Arun Patra <arunkpatra@gmail.com>
2021-04-24 14:09:17 +0530

    Transaction processor

---
+++ transaction-processor/src/main/java/com/reloadly/transaction/processor/ReloadlyTransactionProcessorApp.java
@@ -0,0 +1,13 @@
+package com.reloadly.transaction.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReloadlyTransactionProcessorApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReloadlyTransactionProcessorApp.class, args);
+	}
+
+}
---
+++ transaction-processor/src/test/java/com/reloadly/transaction/processor/ReloadlyTransactionProcessorAppTests.java
@@ -0,0 +1,10 @@
+package com.reloadly.transaction.processor;
+
+import org.junit.jupiter.api.Test;
+
+class ReloadlyTransactionProcessorAppTests extends AbstractIntegrationTest{
+
+	@Test
+	void context_should_load() {
+	}
+}
