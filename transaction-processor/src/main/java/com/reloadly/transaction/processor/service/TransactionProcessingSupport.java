95b788c12e6b499c3c327030b287206ecc235c8a
Arun Patra <arunkpatra@gmail.com>
2021-04-25 22:57:03 +0530

    Txn work in progress

--- transaction-processor/src/main/java/com/reloadly/transaction/processor/service/TransactionProcessingSupport.java
+++ transaction-processor/src/main/java/com/reloadly/transaction/processor/service/TransactionProcessingSupport.java
@@ -1,6 +1,37 @@
 package com.reloadly.transaction.processor.service;
 
 
+import com.reloadly.transaction.entity.TransactionEntity;
+import com.reloadly.transaction.model.TransactionStatus;
+import com.reloadly.transaction.repository.TransactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class TransactionProcessingSupport extends TransactionProcessingNotificationSupport{
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionProcessingSupport.class);
+    private final TransactionRepository transactionRepository;
+
+    protected TransactionProcessingSupport(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    protected void handleAddMoneyTransaction(TransactionEntity te) {
+        // Change status
+        te.setTransactionStatus(TransactionStatus.PROCESSING);
+        te = transactionRepository.save(te);
+
+        //
+        // TODO: Ask the Account microservice to credit the account
+        //
+
+        te.setTransactionStatus(TransactionStatus.SUCCESSFUL);
+        te = transactionRepository.save(te);
+
+    }
+
+
+    protected void handleSendAirtimeTransaction(TransactionEntity te) {
+
+    }
 }
--- transaction-processor/src/main/java/com/reloadly/transaction/processor/service/TransactionProcessorServiceImpl.java
+++ transaction-processor/src/main/java/com/reloadly/transaction/processor/service/TransactionProcessorServiceImpl.java
@@ -1,11 +1,13 @@
 package com.reloadly.transaction.processor.service;
 
 import com.reloadly.transaction.entity.TransactionEntity;
+import com.reloadly.transaction.exception.ReloadlyTxnProcessingException;
 import com.reloadly.transaction.repository.TransactionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import java.util.Optional;
@@ -17,20 +19,34 @@ public class TransactionProcessorServiceImpl extends TransactionProcessingSuppor
     private final TransactionRepository transactionRepository;
 
     protected TransactionProcessorServiceImpl(TransactionRepository transactionRepository) {
+        super(transactionRepository);
         this.transactionRepository = transactionRepository;
     }
 
+    @Transactional
     @KafkaListener(topics = "com.reloadly.inbound.txn.topic", groupId = "inboundTxnProcessingConsumerGrp")
-    public void processInboundTransaction(String txnId) {
+    public void processInboundTransaction(String txnId) throws ReloadlyTxnProcessingException {
         Assert.notNull(txnId, "Transaction ID can not be null");
-
         LOGGER.info("Inbound Transaction received. Txn ID: {}", txnId);
 
         Optional<TransactionEntity> txnOpt = transactionRepository.findByTxnId(txnId);
-
         if (!txnOpt.isPresent()) {
             LOGGER.error("Transaction ID: {} was not found.", txnId);
             return;
+        }
+
+        TransactionEntity txnEntity = txnOpt.get();
+
+        switch (txnEntity.getTransactionType()) {
+            case ADD_MONEY:
+                break;
+
+            case SEND_AIRTIME:
+
+                break;
+
+            default:
+                LOGGER.error("Unknown Transaction type: {}", txnEntity.getTransactionType().name());
         }
 
     }
