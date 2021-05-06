a12886fe5ad7544b3d5252368ce1b3e5059f1285
Arun Patra <arunkpatra@gmail.com>
2021-05-07 00:23:50 +0530

    Kubernetes deployment
    - Updated run.sh scripts for services
    - Updated K8s manifest
    - Updated documentation

--- notification-service/run.sh
+++ notification-service/run.sh
@@ -4,16 +4,44 @@ set -e
 
 RELOADLY_OPTS=
 
-if [ -n "${DB_USER}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.datasource.hikari.username=${DB_USER}"
+if [ -n "${SBA_SERVER_URL}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.boot.admin.client.url=${SBA_SERVER_URL}"
 fi
 
-if [ -n "${DB_PASSWORD}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.datasource.hikari.password=${DB_PASSWORD}"
+if [ -n "${SBA_CLIENT_URL}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.boot.admin.client.instance.service-base-url=${SBA_CLIENT_URL}"
 fi
 
-if [ -n "${DB_URL}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.datasource.url=${DB_URL}"
+if [ -n "${SWAGGER_HOST}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.integration.swagger.host=${SWAGGER_HOST}"
+fi
+
+if [ -n "${AUTH_SVC_ENDPOINT}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${AUTH_SVC_ENDPOINT}"
+fi
+
+if [ -n "${TWILIO_DRY_RUN}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${TWILIO_DRY_RUN}"
+fi
+
+if [ -n "${TWILIO_MSG_SVC_ID}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${TWILIO_MSG_SVC_ID}"
+fi
+
+if [ -n "${TWILIO_ACCOUNT_SID}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${TWILIO_ACCOUNT_SID}"
+fi
+
+if [ -n "${TWILIO_AUTH_TOKEN}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${TWILIO_AUTH_TOKEN}"
+fi
+
+if [ -n "${EMAIL_DRY_RUN}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${EMAIL_DRY_RUN}"
+fi
+
+if [ -n "${SENDER_EMAIL_ID}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${SENDER_EMAIL_ID}"
 fi
 
 echo RELOADLY_OPTS : "$RELOADLY_OPTS"
--- transaction-processor/run.sh
+++ transaction-processor/run.sh
@@ -16,5 +16,21 @@ if [ -n "${DB_URL}" ]; then
   RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.datasource.url=${DB_URL}"
 fi
 
+if [ -n "${SBA_SERVER_URL}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.boot.admin.client.url=${SBA_SERVER_URL}"
+fi
+
+if [ -n "${SBA_CLIENT_URL}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.boot.admin.client.instance.service-base-url=${SBA_CLIENT_URL}"
+fi
+
+if [ -n "${NOTIFICATION_SVC_ENDPOINT}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.notification.reloadly-notification-service-endpoint=${NOTIFICATION_SVC_ENDPOINT}"
+fi
+
+if [ -n "${SVC_ACCT_API_KEY}" ]; then
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.api.transaction.processor.svc-account-api-key=${SVC_ACCT_API_KEY}"
+fi
+
 echo RELOADLY_OPTS : "$RELOADLY_OPTS"
 java $RELOADLY_OPTS -jar /app.jar
