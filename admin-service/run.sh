ba39eb9bccc0c628e1ec2644a149abb3508de211
Arun Patra <arunkpatra@gmail.com>
2021-05-11 01:08:25 +0530

    Bug fixes.

--- admin-service/run.sh
+++ admin-service/run.sh
@@ -17,17 +17,5 @@ else
   RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.profiles.active=${RELOADLY_ENV}"
 fi
 
-if [ -n "${DB_USER}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.datasource.hikari.username=${DB_USER}"
-fi
-
-if [ -n "${DB_PASSWORD}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.datasource.hikari.password=${DB_PASSWORD}"
-fi
-
-if [ -n "${DB_URL}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dspring.datasource.url=${DB_URL}"
-fi
-
 echo RELOADLY_OPTS : "$RELOADLY_OPTS"
 java $RELOADLY_OPTS -jar /app.jar
--- notification-service/run.sh
+++ notification-service/run.sh
@@ -42,27 +42,27 @@ if [ -n "${AUTH_SVC_ENDPOINT}" ]; then
 fi
 
 if [ -n "${TWILIO_DRY_RUN}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${TWILIO_DRY_RUN}"
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.integration.sms.twilio.dry-run=${TWILIO_DRY_RUN}"
 fi
 
 if [ -n "${TWILIO_MSG_SVC_ID}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${TWILIO_MSG_SVC_ID}"
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.integration.sms.twilio.messaging-service-id=${TWILIO_MSG_SVC_ID}"
 fi
 
 if [ -n "${TWILIO_ACCOUNT_SID}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${TWILIO_ACCOUNT_SID}"
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.integration.sms.twilio.twilio-account-sid=${TWILIO_ACCOUNT_SID}"
 fi
 
 if [ -n "${TWILIO_AUTH_TOKEN}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${TWILIO_AUTH_TOKEN}"
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.integration.sms.twilio.twilio-auth-token=${TWILIO_AUTH_TOKEN}"
 fi
 
 if [ -n "${EMAIL_DRY_RUN}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${EMAIL_DRY_RUN}"
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.integration.email.ses.dry-run=${EMAIL_DRY_RUN}"
 fi
 
 if [ -n "${SENDER_EMAIL_ID}" ]; then
-  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.auth.reloadly-auth-service-endpoint=${SENDER_EMAIL_ID}"
+  RELOADLY_OPTS="$RELOADLY_OPTS -Dreloadly.integration.email.ses.from-email-id=${SENDER_EMAIL_ID}"
 fi
 
 echo RELOADLY_OPTS : "$RELOADLY_OPTS"
